a6c489c67151306fcec9ef631739a5ad937150c5
codehunter2000 <gabrielrhunt@gmail.com>
2016-10-28 11:43:00 -0700

    Completed basic class outline as per documentation.

--- CS220/Homework 10/edu/miracosta/cs220/homework10/CodeWriter.java
+++ CS220/Homework 10/edu/miracosta/cs220/homework10/CodeWriter.java
@@ -1,6 +1,40 @@
 package edu.miracosta.cs220.homework10;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class CodeWriter 
 {
+	private String fileLocation;
+	private PrintWriter outputStream;
 	
+	public CodeWriter(String file)
+	{
+		try 
+		{
+			fileLocation = file;
+			outputStream = new PrintWriter(fileLocation);
+		} 
+		
+		catch (FileNotFoundException e) 
+		{
+			System.out.println(e.getLocalizedMessage());
+		}
+	}
+	
+	public void writeArithmetic(String command)
+	{
+		
+	}
+	
+	public void writePopPush(String command, String segment, int index)
+	{
+		
+	}
+	
+	
+	public void close()
+	{
+		outputStream.close();
+	}
 }
