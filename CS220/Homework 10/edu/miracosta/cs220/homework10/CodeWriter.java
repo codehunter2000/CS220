f8f5e83db4c656b89e7663a3329a6578892b73db
codehunter2000 <gabrielrhunt@gmail.com>
2016-10-28 17:52:40 -0700

    Added definitions supporting some arithmetic & push/pop operations.

--- CS220/Homework 10/edu/miracosta/cs220/homework10/CodeWriter.java
+++ CS220/Homework 10/edu/miracosta/cs220/homework10/CodeWriter.java
@@ -7,6 +7,7 @@ public class CodeWriter
 {
 	private String fileLocation;
 	private PrintWriter outputStream;
+	private Parser parser;
 	
 	public CodeWriter(String file)
 	{
@@ -14,6 +15,8 @@ public class CodeWriter
 		{
 			fileLocation = file;
 			outputStream = new PrintWriter(fileLocation);
+			parser = new Parser();
+			
 		} 
 		
 		catch (FileNotFoundException e) 
@@ -24,12 +27,126 @@ public class CodeWriter
 	
 	public void writeArithmetic(String command)
 	{
+		if (command.contains("add"))
+		{
+			String line1 = "@SP";
+			String line2 = "AM = M - 1";
+			String line3 = "D = M";
+			String line4 = "A = A - 1";
+			String line5 = "M = M + D";
+			outputStream.println(line1);
+			outputStream.println(line2);
+			outputStream.println(line3);
+			outputStream.println(line4);
+			outputStream.println(line5);
+		}
 		
+		if (command.contains("eq"))
+		{
+			String line1 = "@SP";
+			String line2 = "A = M - 1";
+			String line3 = "A = M - 1";
+			String line4 = "D = M";
+			String line5 = "A = A + 1";
+			String line6 = "D = D - M";
+			String line7 = "@_1";
+			String line8 = "D;JEQ";
+			String line9 = "@_2";
+			String line10 = "D = 0";
+			String line11 = "0;JMP";
+			String line12 = "(_1)";
+			String line13 = "D = -1";
+			String line14 = "(_2)";
+			String line15 = "@SP";
+			String line16 = "AM = M - 1";
+			String line17 = "A = A - 1";
+			String line18 = "M = D";
+			outputStream.println(line1);
+			outputStream.println(line2);
+			outputStream.println(line3);
+			outputStream.println(line4);
+			outputStream.println(line5);
+			outputStream.println(line6);
+			outputStream.println(line7);
+			outputStream.println(line8);
+			outputStream.println(line9);
+			outputStream.println(line10);
+			outputStream.println(line11);
+			outputStream.println(line12);
+			outputStream.println(line13);
+			outputStream.println(line14);
+			outputStream.println(line15);
+			outputStream.println(line16);
+			outputStream.println(line17);
+			outputStream.println(line18);
+		}
 	}
 	
 	public void writePopPush(String command, String segment, int index)
 	{
+		if (command.contains("push"))
+		{
+			if (segment.contains("constant"))
+			{
+				String line1 = "@" + index;
+				String line2 = "D = A";
+				String line3 = "@SP";
+				String line4 = "AM = M + 1";
+				String line5 = "A = A - 1";
+				String line6 = "M = D";
+				outputStream.println(line1);
+				outputStream.println(line2);
+				outputStream.println(line3);
+				outputStream.println(line4);
+				outputStream.println(line5);
+				outputStream.println(line6);
+			}
+			
+			else if (segment.contains("local"))
+			{
+				
+			}
+			
+			else if (segment.contains("argument"))
+			{
+				
+			}
+		}
 		
+		else if (command.contains("pop"))
+		{
+			if (segment.contains("local"))
+			{
+				String line1 = "@SP";
+				String line2 = "AM = M - 1";
+				String line3 = "D = M";
+				String line4 = "@LCL";
+				String line5 = "A = M + 1";
+				String line6 = "A = A + 1";
+				String line7 = "M = D";
+				outputStream.println(line1);
+				outputStream.println(line2);
+				outputStream.println(line3);
+				outputStream.println(line4);
+				outputStream.println(line5);
+				outputStream.println(line6);
+				outputStream.println(line7);
+			}
+			
+			else if (segment.contains("static"))
+			{
+				String line1 = "@SP";
+				String line2 = "AM = M - 1";
+				String line3 = "D = M";
+				String line4 = "@f." + index;
+				String line5 = "M = D";
+				outputStream.println(line1);
+				outputStream.println(line2);
+				outputStream.println(line3);
+				outputStream.println(line4);
+				outputStream.println(line5);
+			}
+		}
 	}
 	
 	
